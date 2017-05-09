5c8a3b8cd62f817ff75fd0adffde418f30407359
Alex Bennett <no.one.likes.the.internet@gmail.com>
2017-05-09 18:46:02 -0500

    Integrated new and create actions for portfolios

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   get 'pages/contact'
 
+  post'/portfolios/new'
+
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
